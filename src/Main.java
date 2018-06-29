9781fed2d6b3f0ac42e61a65a72b44c41e58648d
GuGB <gustavo_gravena@hotmail.com>
2018-06-28 22:22:43 -0300

    Remoção de importes desnecessários

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.LineNumberInputStream;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
 
 class Main {
 
